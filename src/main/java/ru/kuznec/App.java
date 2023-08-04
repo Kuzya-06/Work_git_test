db9173cde150346f9a7b0c14ea7edffa232f8bff
Kuzya-06 <107077399+Kuzya-06@users.noreply.github.com>
2023-08-04 23:29:18 +0300

    Update App.java
    Student student = new Student(1,"Bob", 56);

--- src/main/java/ru/kuznec/App.java
+++ src/main/java/ru/kuznec/App.java
@@ -6,6 +6,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Misha !!!");
         Student student = new Student(1,"Bob", 56);
+        Student student2 = new Student(2,"Henry", 14);
         System.out.println(student);
+         System.out.println(student2);
     }
 }
