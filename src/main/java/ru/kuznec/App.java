21fb6bd1fb294b9b5b38e1a922d193b705b43c55
Kuzya-06 <kuzya-06@yandex.ru>
2023-08-04 23:14:28 +0300

    Update App.class

--- src/main/java/ru/kuznec/App.java
+++ src/main/java/ru/kuznec/App.java
@@ -1,7 +1,11 @@
 package ru.kuznec;
 
+import ru.kuznec.entity.Student;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Misha !!!");
+        Student student = new Student(1,"Bob", 56);
+        System.out.println(student.toString());
     }
 }
