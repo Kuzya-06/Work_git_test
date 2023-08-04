2764527b90a245883bc48438802898588383088d
Kuzya-06 <kuzya-06@yandex.ru>
2023-08-04 23:21:33 +0300

    Delete .toString in "System.out.println(student)" in App.class

--- src/main/java/ru/kuznec/App.java
+++ src/main/java/ru/kuznec/App.java
@@ -6,6 +6,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Misha !!!");
         Student student = new Student(1,"Bob", 56);
-        System.out.println(student.toString());
+        System.out.println(student);
     }
 }
